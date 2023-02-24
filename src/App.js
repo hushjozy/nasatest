a25a2eec79cd9e7d69e597ad8f92f328d6372f50
Joseph <hushjoezzybad@gmail.com>
2023-02-24 19:01:52 +0100

    deploy

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
-import logo from "./logo.svg";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Routes, Route } from "react-router-dom";
-
 import "./App.css";
 import Show from "./pages/show/Show";
 import Search from "./pages/search/Search";
-
-// ends
-// "/search
-// "/asset/{nasa_id}
-// "/metadata/{nasa_id}
-// "/captions/{nasa_id}
-// "/album/{album_name}
 
 function App() {
   return (
